d688070bac4c0958ab2b970ca839871657f33609
rs123456raushan <rs123456raushan@gmail.com>
2024-11-25 13:17:21 +0530

    added form data saving functionality in google sheet

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { tickIcon } from "../assets/icons";
 
 const Form = () => {
 
-  const scriptURL =
-    "https://script.google.com/macros/s/AKfycbyMo88J42CM0E8qpeSCMWb3uOY67lv4D-kkpGewYw2T5qmn9LOtkYlcQbXkXSwEwAr5/exec";
-  const form = document.forms["contact-us"];
+  const [name, setName] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [phone, setPhone] = useState(null)
+  const [requirements, setRequirements] = useState(null)
+
+  const scriptURL = "https://api.sheetbest.com/sheets/4399bc0d-fdbc-4514-9f47-42c579dbc912"
 
   const submitHandler = async (e) => {
     e.preventDefault();
     fetch(scriptURL, {
       method: "POST",
       headers: {
+        'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: new FormData(form)
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        phone: phone,
+        requirements: requirements
+      })
     })
       .then((response) => {
         alert("Form Submitted Successfully!");
@@ -57,13 +66,13 @@ const Form = () => {
           name="contact-us"
           onSubmit={submitHandler}
         >
-          <input type="text" name="name" placeholder="Enter your name" />
-          <input type="email" name="email" placeholder="Enter your email" />
+          <input onChange={(e) => setName(e.target.value)} type="text" name="name" placeholder="Enter your name" />
+          <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="Enter your email" />
           <div>
             <span>+91</span>
-            <input type="text" name="phone" placeholder="Enter your phone no." />
+            <input onChange={(e) => setPhone(e.target.value)} type="text" name="phone" placeholder="Enter your phone no." />
           </div>
-          <textarea name="requirements" rows={5} placeholder="Describe your requirement"></textarea>
+          <textarea onChange={(e) => setRequirements(e.target.value)} name="requirements" rows={5} placeholder="Describe your requirement"></textarea>
           <button className="btn" type="submit">
             Get a Call Back
           </button>
