18e3c638aecab84b64977f4ac07dc2edc946c4ed
rs123456raushan <rs123456raushan@gmail.com>
2024-11-25 12:01:15 +0530

    added google sheet code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,27 @@ import React from "react";
 import { tickIcon } from "../assets/icons";
 
 const Form = () => {
+
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbyMo88J42CM0E8qpeSCMWb3uOY67lv4D-kkpGewYw2T5qmn9LOtkYlcQbXkXSwEwAr5/exec";
+  const form = document.forms["contact-us"];
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    fetch(scriptURL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: new FormData(form)
+    })
+      .then((response) => {
+        alert("Form Submitted Successfully!");
+        console.log("Success!", response);
+      })
+      .catch((error) => console.error("Error!", error.message));
+  };
+
   return (
     <div className="heroWrapper formWrapper">
       <div className="formLeft">
@@ -32,14 +53,17 @@ const Form = () => {
       <div className="heroRight formRight">
         <h5>Get a call back in 30 mins.</h5>
         <p>Share your Requirements or WhatsApp me to get a call back.</p>
-        <form>
-          <input placeholder="Enter your name" />
-          <input placeholder="Enter your email" />
+        <form
+          name="contact-us"
+          onSubmit={submitHandler}
+        >
+          <input type="text" name="name" placeholder="Enter your name" />
+          <input type="email" name="email" placeholder="Enter your email" />
           <div>
             <span>+91</span>
-            <input placeholder="Enter your phone no." />
+            <input type="text" name="phone" placeholder="Enter your phone no." />
           </div>
-          <textarea rows={5} placeholder="Describe your requirement"></textarea>
+          <textarea name="requirements" rows={5} placeholder="Describe your requirement"></textarea>
           <button className="btn" type="submit">
             Get a Call Back
           </button>
